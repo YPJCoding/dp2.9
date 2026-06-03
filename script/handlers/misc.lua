21915fa56d3cd39c04b44edeb53a57f895df6164
YPJCoding <178675502@qq.com>
2026-06-03 21:56:04 +0800

    chore: add misc handler template

---
+++ script/handlers/misc.lua
@@ -0,0 +1,15 @@
+-- 其他零散道具 handler 模板
+--
+-- 当前文件仅提供模块边界，暂不注册任何现有业务逻辑。
+
+local M = {}
+
+function M.register(item_handler, ctx)
+    -- 后续迁移：
+    -- - 异界重置券
+    -- - 装备设计图熟练度
+    -- - 角色出战
+    -- - 其他暂不适合归类的道具券
+end
+
+return M
