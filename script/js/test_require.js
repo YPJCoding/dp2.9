476aac8c56267a31a66fbe71291d5ce82c983a2b
YPJCoding <178675502@qq.com>
2026-06-05 10:00:32 +0800

    test: simplify test file to basic console.log

--- script/js/test_require.js
+++ script/js/test_require.js
@@ -1,4 +1,2 @@
 // TEST: 验证 api_read_file + eval 能否加载并执行外部 JS 文件
-// 引用 df_game_r.js 中的全局函数 getCurSec 来验证外部代码可访问全局作用域
-var ts = typeof getCurSec === 'function' ? getCurSec(null) : -1;
-console.log('[test_eval_file] loaded! getCurSec=' + ts);
+console.log('[test_eval_file] loaded successfully!');
