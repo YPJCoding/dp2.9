5402c78aaff5b000505c2ae925b2aaf8309c6fa3
YPJCoding <178675502@qq.com>
2026-06-05 10:14:23 +0800

    test: minimal let keyword support test

--- script/js/test_require.js
+++ script/js/test_require.js
@@ -1,44 +1,7 @@
-// TEST: ES6+ 语法支持测试
-var results = [];
-
-// 1. 箭头函数
+// TEST: let 关键字支持
+var ok = false;
 try {
-    var fn = (a, b) => a + b;
-    results.push('arrow:OK(' + fn(1, 2) + ')');
-} catch(e) { results.push('arrow:FAIL'); }
-
-// 2. let / const
-try {
-    let x = 10; const y = 20;
-    results.push('let/const:OK(' + (x + y) + ')');
-} catch(e) { results.push('let/const:FAIL'); }
-
-// 3. 模板字符串
-try {
-    var name = 'test';
-    results.push('template:OK(' + ('hello ' + name) + ')');
-} catch(e) { results.push('template:FAIL'); }
-
-// 4. 解构
-try {
-    var arr = [1, 2]; var a = arr[0]; var b = arr[1];
-    results.push('destructure:OK(' + a + ',' + b + ')');
-} catch(e) { results.push('destructure:FAIL'); }
-
-// 5. class
-try {
-    var TestClass = function TestClass(name) { this.name = name; };
-    var inst = new TestClass('test');
-    results.push('class:OK(' + inst.name + ')');
-} catch(e) { results.push('class:FAIL'); }
-
-// 6. Promise
-try {
-    if (typeof Promise !== 'undefined') {
-        results.push('Promise:OK');
-    } else {
-        results.push('Promise:N/A');
-    }
-} catch(e) { results.push('Promise:FAIL'); }
-
-console.log('[test_es6] ' + results.join(', '));
+    eval('let x = 1; var r = x;');
+    ok = true;
+} catch(e) {}
+console.log('[test_es6] let=' + (ok ? 'OK' : 'FAIL'));
